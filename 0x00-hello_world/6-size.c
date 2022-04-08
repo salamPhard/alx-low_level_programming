90ee0dda16d582497593a751fa750a060b5907ea
salamPhard <salamfadilu@gmail.com>
2022-04-08 07:49:25 -0700

    size of variables

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * Main - main block
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf(("Size of int : %i byte(s)\n"),sizeof(int));
+	printf(("Size of char : %i byte(s)\n", sizeof(char));
+	printf(("Size of long int: %i byte(s)\n",sizeof(long int));
+	printf(("Size of long long int : %i byte(s)\n", sizeof(long long int));
+	printf(("Size of float : %i byte(s)\n", sizeof(float));
+	return (0);
+}
+
