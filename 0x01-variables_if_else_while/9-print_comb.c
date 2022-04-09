0e183c46b1ee0b088733f5e5ef38a65b8d93543b
salamPhard <salamfadilu@gmail.com>
2022-04-09 16:46:55 -0700

    adding comma to single digit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: print all single digits with comma
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
+		if (n != 9)
+		{
+			putchar(',');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
