c938325b8d272f979c855894d91d60d8f90bf165
salamPhard <salamfadilu@gmail.com>
2022-04-10 03:24:42 -0700

    statement

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Printing a statment to standard output
+ * Return: 0
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful\" \- Dora Korpar, 2015-10-19");
+	return (1);
+}
