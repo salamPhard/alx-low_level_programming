1280efbfeb58d90bb0e21c790e999e1e02bbe857
salamPhard <salamfadilu@gmail.com>
2022-04-12 15:15:46 -0700

    using putchar to print statement

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - main block
+ * Description: Writing a putchar to stdout
+ * Return: 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
