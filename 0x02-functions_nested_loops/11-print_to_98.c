9ace2885d2bd01c97c3b000604a9ce8af46e9dbd
salamPhard <salamfadilu@gmail.com>
2022-04-13 04:51:57 -0700

    add print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * print_to_98 - printing a value to 98
+ * @n: first parameter
+ *
+ * Description: Print to 98 from all numbers
+ * Return: 0
+ */
+ void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			_putchar(i + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			_putchar(i + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else
+	{
+		_putchar(9 + '0);
+		_putchar(8 + '0');
+	}
+}
+
