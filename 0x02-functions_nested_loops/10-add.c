e2be325773b6240942e81f0f0c1617cc0cfd00b4
salamPhard <salamfadilu@gmail.com>
2022-04-12 22:45:40 -0700

    test add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,15 +2,13 @@
 #include<stdlib.h>
 /**
  * add - adding two integers
- * 
+ * @a: first parameter
+ * @b: second parameter
+ *
  * Description: adding two integers and calling
  * Return: 0;
  */
-int add(int int)
+int add(int a, int b)
 {
-	int first_num, second_num, result;
-
-	result = first_num + second_num;
-
-	return (result);
+	return (a + b);
 }
