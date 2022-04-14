ca8073221ff5789a0c84d711c0bb38ba52bdd244
salamPhard <salamfadilu@gmail.com>
2022-04-13 17:34:16 -0700

    edit print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,61 +8,17 @@
  */
  void print_to_98(int n)
 {
-	int i;
-
-	if (n < 98)
+	while ( n < 98)
 	{
-		for (i = n; i <= 98; i++)
-		{
-			if (i < 0)
-			{
-				_putchar('-');
-				_putchar((i * -1) + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			if (i >= 0 && i < 10)
-			{
-				_putchar((i % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			if (i >= 10 && i <= 98)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
+		printf("%i, ", n);
+		n++;
 	}
-	if (n > 98)
+	while (n > 98)
 	{
-		for (i = n; i >= 98; i--)
-		{
-			if (i >= 100)
-			{
-				_putchar((i / 100) + '0');
-				_putchar((i - 100) / 10 + '0');
-				_putchar((i % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			if (i < 100)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
+		printf("%i, ", n);
+		n--;
 	}
-	if (n == 98)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	_putchar('\n');
-
+	printf("98");
+	putchar('\n');
 }
 
