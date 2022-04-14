a2e18b3c831950ece931de40ab4d595d75121736
salamPhard <salamfadilu@gmail.com>
2022-04-13 17:24:17 -0700

    multiple  code

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include<stdlib.h>
+/**
+ * main - main block
+ * Description: prints all multiples of 3 and 5
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+	int sum = 0;
+
+	while (c < 1024)
+	{
+		if (c % 3 == 0 || c % 5 == 0)
+		{
+			sum = sum + c;
+		}
+
+		c++;
+	}
+	printf("%i\n", sum)
+	
+	return (0);
+}
