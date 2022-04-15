b78648fe2e10e7ca85a02800a6c607d4a0d3c6e0
salamPhard <salamfadilu@gmail.com>
2022-04-15 09:04:37 -0700

    tested

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_square - create a square function
+ * @n: parameter
+ */
+void print_square(int n)
+{
+	int i = 0, k;
+
+	while (i < n && n > 0)
+	{
+		k = 0;
+		while(k < n)
+		{
+			_putchar('#');
+			k++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+	if (i == 0)
+		_putchar('\n');
+}
