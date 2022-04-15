295a9965fb20012feeb8f6de7795479080204b6f
salamPhard <salamfadilu@gmail.com>
2022-04-14 21:45:19 -0700

    edit end to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * print_numbers - print numbers from 0 to 9
- * Return: void
+ * Return: 0
  */
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 90; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		putchar((i % 10) + '0');
 	}
