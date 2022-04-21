5893fdc7eeed5a1ae7098c93b758169880da7b11
salamPhard <salamfadilu@gmail.com>
2022-04-21 12:23:07 -0700

    print aray

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcpy - printing an array
+ * @dest: first parameter
+ * @src: second parameter
+ *
+ * Return: pointer
+ */
+char *strcpy(char *dest, char *str)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = src[i];
+	return (dest);
+}
+
