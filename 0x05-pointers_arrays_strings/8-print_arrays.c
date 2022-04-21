8f9c85be99ad7555f6bdfbb51066650058af9290
salamPhard <salamfadilu@gmail.com>
2022-04-20 21:58:27 -0700

    array

---
+++ 0x05-pointers_arrays_strings/8-print_arrays.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * print_array - printing arrays up to n
+ * @a: read from array
+ * @n: number of element to read
+ * Return: 0
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; n > i; i++)
+		{
+			if (i != n - 1)
+			{
+				printf("%d",  a[i]);
+			}
+			else
+			{
+				printf("%d", a[i]);
+			}
+		}
+	}
+	printf("\n");
+}
