87aa09589b55355543c62c89377ae010ad9c0703
salamPhard <salamfadilu@gmail.com>
2022-04-20 21:43:14 -0700

    print a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - printing character
+ * @str - parameter
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
