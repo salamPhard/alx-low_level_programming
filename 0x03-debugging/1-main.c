7b9a4d8de29c4e01a96dd35a067f60f4be780b1a
salamPhard <salamfadilu@gmail.com>
2022-04-24 13:22:24 -0700

    push pending commit

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - infitie loop cause
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming in:\n");
+
+	i = 0;
+
+	/** while (i < 10)
+	 * {
+	 * 	putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided!\\o/\n");
+
+	return (0);
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-My C crash course low level repo
