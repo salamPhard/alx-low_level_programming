1cd708dc1969e127d7fe095616fe4d316b0e345b
salamPhard <salamfadilu@gmail.com>
2022-04-24 13:07:29 -0700

    test strincat n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncat - concatenate two strings
+ * @dest: first parameter
+ * @src: second parameter
+ * @n: int parameter
+ *
+ * Return: 0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, len = 0;
+
+	while (dest[len] != '\0')
+	{
+		++len;
+	}
+	while (src[i] != '\0' && i < n)
+	{
+		dest[len] = src[i];
+		++len;
+		++i;
+	}
+	dest[len] = '\0';
+	return (dest);
+}
