7ee4511f5e6c9927628794320b14b4740fa8e707
salamPhard <salamfadilu@gmail.com>
2022-04-24 06:49:55 -0700

    first value

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _astrcat - concatenate two string
+ * @dest: first parameter
+ * @src: second parameter
+ *
+ * Return: 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, len;
+
+	len = 0;
+	while (dest[len] != '\0')
+	{
+		++len;
+	}
+	for (i = 0; src[i] != '\0'; ++i; ++len)
+	{
+		dest[len] = src[i];
+	}
+	dest[len] = ='\0';
+	return (dest);
+}
+ 
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,2 @@
+_putchar(char *s);
+char *_strcat(char *dest, char *src);
