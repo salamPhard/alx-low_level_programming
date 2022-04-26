322d842073e99a840d13e8eea64377c384c796b4
salamPhard <salamfadilu@gmail.com>
2022-04-26 13:51:45 -0700

    creating readme, headerfile and oda file

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * memset - copy a char in an n area of array
+ * @s: pointer to the array
+ * @b: character to be replaced
+ * @n: number of n times
+ *
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i =0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Working mmore on Arrays and pointers
