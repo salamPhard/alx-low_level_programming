4dae9a0a1e42894422407003aa3c8f40579069d5
salamPhard <salamfadilu@gmail.com>
2022-04-27 21:39:45 -0700

    final edit

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - set a string
+ * @s: string to set
+ * @to: direction of setting
+ *
+ * Return: 0
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
