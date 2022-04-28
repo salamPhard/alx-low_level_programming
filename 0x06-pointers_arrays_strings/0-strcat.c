0692729c942d22f73d3d7429df058a3e27100e89
salamPhard <salamfadilu@gmail.com>
2022-04-27 22:00:42 -0700

    edit error

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,7 +9,7 @@
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
-	
+
 	for (i = 0; dest[i] != '\0'; i++);
 	for (j = 0; src[j] != '\0'; j++)
 	{
@@ -18,4 +18,3 @@ char *_strcat(char *dest, char *src)
 		dest[i + j] = '\0';
 	return (dest);
 }
- 
