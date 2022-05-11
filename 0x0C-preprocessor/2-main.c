94c91c35263aad89901b705b1c51851985b43577
salamPhard <salamfadilu@gmail.com>
2022-05-11 16:06:43 -0700

    preprocessors work

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+ * main - prints the name of file
+ * Description: Printing the name of a file
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}	
