47ca818494590dea8cd6f61412e5dc25973862d3
salamPhard <salamfadilu@gmail.com>
2022-06-05 03:37:51 -0700

    edit variables

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	register int_open, _read, _write;
+	register int _open, _read, _write;
 	register char *buffer = malloc(sizeof(char) * letters);
 
 	if (!(buffer))
