e2c13983735327be1b6735c179fc55072898299a
salamPhard <salamfadilu@gmail.com>
2022-06-05 04:32:03 -0700

    edit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	_file = open(filename, O_WRONLY, O_APPEND);
-	if (_FILE == -1)
+	if (_file == -1)
 	{
 		return (-1);
 	}
